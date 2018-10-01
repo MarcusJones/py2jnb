2a5d0351d4cbb4cc8a4c0d41713333237bf7346e
MarcusJones <jones.0bj3@gmail.com>
2018-10-01 12:21:06 +0200

    update version

--- setup.py
+++ setup.py
@@ -35,6 +35,6 @@ setuptools.setup(
     # test_suite='tests',
     # tests_require=test_requirements,
     url='https://github.com/MarcusJones/py2jnb',
-    version='0.0.3',
+    version='0.0.4',
     # zip_safe=False,
 )
