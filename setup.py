b55aefde20b807e7dc5db36dede268231c0935c0
MarcusJones <jones.0bj3@gmail.com>
2018-10-01 10:25:51 +0200

    bumpver

--- setup.py
+++ setup.py
@@ -35,6 +35,6 @@ setuptools.setup(
     # test_suite='tests',
     # tests_require=test_requirements,
     url='https://github.com/MarcusJones/py2jnb',
-    version='0.0.2',
+    version='0.0.3',
     # zip_safe=False,
 )
